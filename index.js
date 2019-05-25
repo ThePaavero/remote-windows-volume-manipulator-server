2c3f1aa7ec63bdcc2f8961841515c22d8435a4b5
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-05-25 19:39:13 +0300

    Some DRY fixing plus semantic changes.

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
 const express = require('express')
-const app = express()
-const port = 4004
 const exec = require('child_process').exec
 
+const app = express()
+const port = 4004
+
 /**
+ * Server script that executes a Windows application (see note below) with which one can manipulate Windows' audio volume.
+ *
  * NOTE: This server script needs "SetVol.exe" to be located one directory above this one's.
  * @link https://rlatour.com/setvol/
  */
 
 app.use(express.json())
 
+const getTimeString = () => {
+  const date = new Date()
+  return date.toLocaleDateString() + ' ' + date.toLocaleTimeString()
+}
+
+
 app.post('/', (req, res) => {
 
   /*
+
   Example POST input:
     {
       "action": {
-        "type": "setVolume",
-        "value": 20
+        "type": "SET_VOLUME",
+        "payload": 20
       }
     }
+
    */
 
   const postBody = req.body
   const type = postBody.action.type
-  const value = postBody.action.value
+  const payload = postBody.action.payload
   const exePath = '..\\.\\SetVol.exe'
 
   switch (type) {
-    case 'setVolume':
+    case 'SET_VOLUME':
     default:
-      exec(`${exePath} ${value}`, (error) => {
+      exec(`${exePath} ${payload}`, (error) => {
         if (error !== null) {
-          console.log('exec error: ' + error)
           // Bad. Return early and bail.
+          console.log(getTimeString(), 'Executing failed: ' + error)
           return res.json({
             success: false,
             error: error.message,
@@ -42,9 +53,9 @@ app.post('/', (req, res) => {
         // Good. Send something useful back.
         const debugObject = {
           type,
-          value
+          payload
         }
-        console.log(new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(), debugObject)
+        console.log(getTimeString(), debugObject)
         res.json({
           success: true,
           debugObject
