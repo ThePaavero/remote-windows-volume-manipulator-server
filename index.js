91b6b6043b6abf62163d66a8f460b3d19e4576a9
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-05-25 18:58:05 +0300

    Works.

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 4004
+const exec = require('child_process').exec
 
 app.use(express.json())
 
 app.post('/', (req, res) => {
 
+  /*
+  Example POST input:
+    {
+      "action": {
+        "type": "setVolume",
+        "value": 20
+      }
+    }
+   */
+
+  const type = req.body.action.type
+  const value = req.body.action.value
+
+  switch (type) {
+    case 'setVolume':
+      exec(`..\\.\\SetVol.exe ${value}`, (error) => {
+        if (error !== null) {
+          console.log('exec error: ' + error)
+        }
+      })
+      break
+  }
+
   res.json({
-    success: true
+    success: true,
+    debugObject: {
+      type,
+      value
+    }
   })
 })
 
